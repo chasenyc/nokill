1cd6abfeb7d171bb7ba22601d321065a3bd63216
Alex Harris <chasenyc@gmail.com>
2015-12-26 21:28:52 -0500

    Add owner model.

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,2 +1,35 @@
 class Owner < ActiveRecord::Base
+  attr_reader :password
+
+  validates :email, :password_digest, :session_token,
+            :birthdate, presence: true
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by_email(email)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def password
+    @password
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
 end
