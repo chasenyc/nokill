5ebd8d57aa6a5dcb088504ad86f70b1fea213559
Alex Harris <chasenyc@gmail.com>
2015-12-30 13:14:45 -0500

    Add species table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230175404) do
+ActiveRecord::Schema.define(version: 20151230180159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20151230175404) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "species_id", null: false
+  end
+
+  create_table "species", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
