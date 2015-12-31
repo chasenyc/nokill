39c644d49871c04c3a96b4b1804d39df22477b3a
Alex Harris <chasenyc@gmail.com>
2015-12-30 21:42:50 -0500

    Add animal avatar default and paperclip specifications in model.

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,9 @@
 class Animal < ActiveRecord::Base
+
+  has_attached_file :image,
+                    :styles => {
+                      :thumb => "100x100",
+                      :medium => "300x300"
+                    },
+                    default_url: "animalavatar.jpg"
 end
