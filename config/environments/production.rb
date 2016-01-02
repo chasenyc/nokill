2ee461817244fe86f6dc00f06e0cecad749c3ed3
Alex Harris <chasenyc@gmail.com>
2016-01-01 22:19:32 -0500

    Add mailer settings to production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,22 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.action_mailer.default_url_options = { :host => "www.nokill.nyc" }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@example.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.zoho.com',
+    port:                 587,
+    domain:               'nokill.nyc',
+    user_name:            'no-reply@nokill.nyc',
+    password:             ENV["no_reply_password"],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
+
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
